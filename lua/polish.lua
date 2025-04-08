719499ef29d32a5fec3219b244302797482c6734
kogora <it.is.mr.lag@gmail.com>
2025-04-08 17:56:35 +0300

    add pynvim

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,8 +1,12 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- NOTE: 
+-- 1. python -m venv ~/.nvim_venv
+-- 2. source ~/.nvim_venv/bin/activate
+-- 3. pip install pynvim
+vim.g.python3_host_prog = vim.fn.expand("~/.nvim_venv/bin/python")
 
 -- Set up custom filetypes
 vim.filetype.add {
